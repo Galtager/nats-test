272bdb23cc5aaebc09b0e3a7e5480b22e370c3dc
gal tager <gal@alterno.io>
2024-05-15 13:45:52 +0300

    fix: change publish method to return a promise

--- src/events/custom-publisher.ts
+++ src/events/custom-publisher.ts
@@ -15,9 +15,15 @@ export abstract class CustomPublisher<T extends Event> {
     constructor(client: Stan) {
         this.client = client
     }
-    publish(data: T['data']) {
-        this.client.publish(this.subject, JSON.stringify(data), () => {
-            console.log("Event published");
+    publish(data: T['data']): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client.publish(this.subject, JSON.stringify(data), (err) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve()
+            })
+
         })
     }
 }
--- src/publisher.ts
+++ src/publisher.ts
@@ -7,14 +7,19 @@ const stan = nats.connect('test', 'abc', {
     url: 'http://localhost:4224',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log("Publisher connected to NATS")
 
-    const publisher = new ItemCreatedPublisher(stan)
-    publisher.publish({
-        id: '123',
-        title: 'connect',
-        price: 20
+    const publisher = new ItemCreatedPublisher(stan);
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'connect',
+            price: 20
 
-    })
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
 })
